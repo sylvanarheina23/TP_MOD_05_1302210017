009585ff004fb2b3e03a6fe12f048507021652e7
raliauliya <127048576+sylvanarheina23@users.noreply.github.com>
2023-03-20 00:03:28 +0700

    menambahkan class data generic

--- TP_MOD5_1302210017/TP_MOD5_1302210017/Program.cs
+++ TP_MOD5_1302210017/TP_MOD5_1302210017/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+public class DataGeneric<T>
+{
+    private T Data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + Data);
+    }
+
+    
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Masukkan NIM :");
+        string nim = Console.ReadLine();
+        DataGeneric<string> data = new DataGeneric<string>(nim);
+        data.PrintData();
+    }
+}
 
